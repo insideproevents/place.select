c7f8d4da32de7e9500dd18dc648e75cf5c7a2bd3
Ignacio <insideproevents@gmail.com>
2026-05-07 17:11:25 -0400

    style(hero): move content 10px right

--- app/src/sections/Hero.tsx
+++ app/src/sections/Hero.tsx
@@ -23,7 +23,7 @@ export default function Hero() {
       </div>
 
       {/* Content */}
-      <div className="relative z-10 max-w-[1280px] ml-[-35px] px-4 sm:px-6 lg:px-12 pb-12 sm:pb-16 lg:pb-20 w-full">
+      <div className="relative z-10 max-w-[1280px] ml-[-25px] px-4 sm:px-6 lg:px-12 pb-12 sm:pb-16 lg:pb-20 w-full">
         <div className="max-w-2xl">
           <motion.h1
             initial={{ opacity: 0, y: 30 }}
